86b037041c581f54ea0b891f440dc7d75658d8c7
ojogbon <40061054+ojogbon@users.noreply.github.com>
2020-12-16 11:06:53 -0800

    fix init to suite heroku

--- controllers/init.php
+++ controllers/init.php
@@ -7,8 +7,9 @@ session_start();
 $dev_path = "pro";
 $companyname = "CompanyName";
 
-$parent_path = $dev_path == "dev" ? "wecanchat/" : "app/";
+$parent_path = $dev_path == "dev" ? "wecanchat/" : "";
 
 
     include ($_SERVER['DOCUMENT_ROOT']."/". $parent_path.'models/MainModel.php');
+    echo $_SERVER['DOCUMENT_ROOT'];
     $mainModel = new MainModel();
